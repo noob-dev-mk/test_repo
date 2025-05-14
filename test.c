d72a68ec4644452bfc4d6f854f7442a22b643a07
Musheer <musheer.34512@gmail.com>
2025-05-14 21:01:18 +0530

    V3 4 u

--- test.c
+++ test.c
@@ -2,7 +2,8 @@
 
 int main(){
     printf("This is a test code but I don't want to leave it empty\n");
-    printf("This is version V2");
+    printf("This is version V2\n");
     //easter egg
+    printf("V3 is here!\n");
     return 0;
 }
