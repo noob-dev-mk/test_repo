daa2e6ea8e95e92a136019e2f0fe33e91933cc07
Musheer <musheer.34512@gmail.com>
2025-05-14 20:46:21 +0530

    This is V2

--- test.c
+++ test.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main(){
-    printf("This is a test code but I don't want to leave it empty");
+    printf("This is a test code but I don't want to leave it empty\n");
+    printf("This is version V2");
+    //easter egg
     return 0;
 }
